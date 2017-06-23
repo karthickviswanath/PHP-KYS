c1d838bf3757a94ff5c19df43e5da8368397a7f8
gokulnathpalanivel <gokulnathpalanivel@gmail.com>
2017-06-23 00:42:19 -0700

    Refactor results section with maps displayed based on device width

--- Result.php
+++ Result.php
@@ -18,16 +18,14 @@ $res = "";
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-		$imgID = $row["profileimageid"];
+	$imgID = $row["profileimageid"];
 	$res = $res."<div class='rstBx'><div class='pic'><img src='http://leisurehomes.co/KYS/images/".$imgID.".jpg' alt='Image'/></div><div class='dtm'><a href='index.php'>"
                .$row["CompanyName"]."</a><div class='dtls'>"
                .$row["Description"]."</div><div><i class='fa fa-address-card'></i>" 
 	       .$row["Address"]."</div><div><i class='fa fa-phone'></i>"
-               .$row["Contact"]."</div><button type='button'>Rate this</button><button type='button'>eMail</button></div><div class='dtm'>".$row["Address"]                
-                ."</div>"
-                .'<div><iframe src="https://maps.google.com/maps?hl=en&q='.$row["Address"].'&ie=UTF8&t=m&z=16&iwloc=B&output=embed" frameborder="0" style="border:0" allowfullscreen></iframe></div>'
-                ."</div><div style='clear:both'></div>"
-                ;
+               .$row["Contact"]."</div><div class='actBtn'><button type='button'>Rate this</button><button type='button'>eMail</button></div></div>"
+               .'<div class="mapFr"><iframe src="https://maps.google.com/maps?hl=en&q='.$row["Address"].'&ie=UTF8&t=m&z=16&iwloc=B&output=embed" frameborder="0" style="border:0" allowfullscreen></iframe></div>'
+               ."<div style='clear:both'></div></div>";
     }
     echo $res;
 } else {
